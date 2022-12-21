a5dc90fa6938e6d309e6b1a211c28d4ba10c1a52
Hannah Mullah <hannahmullah@hotmail.com>
2022-12-21 20:22:23 +0000

    completed counter app in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import refresh from './refresh.svg';
 import './App.css';
 
+//using the STATE HOOK
+
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const increase = () => {
+    setCounter(counter => counter + 1)
+  }
+  const decrease = () => {
+    setCounter(counter => counter - 1)
+  }
+  const resetCounter = () => {
+    setCounter(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+
+      <div className='container'>
+        <p>Counter</p>
+
+        <div className='count'>
+          <h1>{counter}</h1>
+        </div>
+        <div className='buttons'>
+          <button onClick={decrease}>-</button>
+
+          <button onClick={increase}>+</button>
+
+          <button onClick={resetCounter}>
+            <img className='rImg' src={refresh}>
+            </img>
+          </button>
+        </div>
+
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
